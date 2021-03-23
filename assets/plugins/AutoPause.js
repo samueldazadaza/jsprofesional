e2385a2ed9d62e8bad6249f590e54ce49111a90e
Samuel Daza <samueldazadaza@gmail.com>
2021-03-22 21:19:52 -0500

    video 22: VisibilityChange

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -1,7 +1,8 @@
 class AutoPause {
     constructor() {
         this.threshold = 0.25;
-        this.handleIntersection = this.handleIntersection.bind(this)
+        this.handleIntersection = this.handleIntersection.bind(this);
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
     }
     run(player) {
         this.player = player;
@@ -10,15 +11,26 @@ class AutoPause {
         })
 
         observer.observe(this.player.media)
+
+        document.addEventListener("visibilitychange", this.handleVisibilityChange)
     }
 
     handleIntersection(entries) {
         const entry = entries[0];
 
-        const isVisible = entry.intersectionRatio  >= this.threshold
+        const isVisible = entry.intersectionRatio >= this.threshold;
 
         if (isVisible) {
             this.player.play()
+        } else {
+            this.player.pause()
+        }
+    }
+
+    handleVisibilityChange() {
+        const isVisible = document.visibilityState === 'visible';
+        if (isVisible) {
+           this.player.play() 
         } else {
             this.player.pause()
         }
