41cf13fc95ef6a6b2e688463ea40a6e2f89a7122
Samuel Daza <samueldazadaza@gmail.com>
2021-03-23 23:18:56 -0500

    video 23: service workes // cambio de index.html agregando comentarios

--- assets/index.js
+++ assets/index.js
@@ -5,12 +5,23 @@ import AutoPause from './plugins/AutoPause.js';
 const video = document.querySelector('video');
 const player = new MediaPlayer({
     el: video, 
-    plugins: [
-    new AutoPlay(),
-    new AutoPause()
-    ],
+    plugins: [new AutoPlay(), new AutoPause()],
 });
 
-const button = document.querySelector('button');
-button.onclick = () => player.togglePlay();
+const playButton = document.querySelector('#playButton');
+playButton.onclick = () => player.togglePlay();
 
+const muteButton = document.querySelector('#muteButton');
+muteButton.onclick = () => {
+    if (player.media.muted) {
+        player.unmute();
+    } else {
+        player.mute();
+    }
+};
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').catch(error => {
+        console.log(error.message);
+    });
+}
