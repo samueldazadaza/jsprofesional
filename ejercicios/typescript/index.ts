43d15ad3583d442580895de90e56725bf57c99a3
Samuel Daza <samueldazadaza@gmail.com>
2021-03-24 20:43:30 -0500

    fin v26: funciones

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -50,5 +50,35 @@ comodin = { type: 'WildCard'};
 // Object
 let someObject: object = { type: 'Wilcard' };
 
+// VIDEO 26 ------------ FUNCIONES
+//FUNCIONES
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+const sum = add(4, 6)
+
+function createAdder (a: number): (number) => number {
+    return function (b: number) {
+        return b + a;
+    }
+}
+
+const addFour = createAdder(4)
+const fourPlus6 = addFour(6);
+
+function fullName (firstName: string, lastName: string = "Smith"): string {
+    return `${firstName} ${lastName}`;
+}
+
+const richard = fullName('Agente:');
+console.log(richard);
+
+
+
+
+
+
+
 
 
