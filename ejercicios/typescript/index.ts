0ee0a0ee8c0f5c68d1665ae5766fff7504b3af21
Samuel Daza <samueldazadaza@gmail.com>
2021-03-24 21:06:53 -0500

    fin v27: interfases (clases)

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -74,6 +74,52 @@ function fullName (firstName: string, lastName: string = "Smith"): string {
 const richard = fullName('Agente:');
 console.log(richard);
 
+// VIDEO 27 ------------ interfaces
+// interfaces
+enum Color {
+    Rojo = "Rojo",
+    Verde = "Verde",
+}
+
+interface Rectangulo {
+    ancho: number;
+    alto: number;
+    color: Color;
+}
+
+let rect: Rectangulo = {
+    ancho: 4,
+    alto: 6,
+    color: Color.Rojo,
+};
+
+function area(r: Rectangulo): number {
+    return r.alto * r.alto;
+}
+
+const areaRect = area(rect);
+console.log(areaRect);
+
+rect.toString = function() {
+    return this.color ? `Un rectangulo ${this.color}` : `Un rectangulo`;
+};
+
+console.log(rect.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
