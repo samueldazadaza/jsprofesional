f95be902066f27dc1709a37f43fcb110aa5d769b
Samuel Daza <samueldazadaza@gmail.com>
2021-03-24 20:22:42 -0500

    fin v25: tipos basicos typescript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,54 @@
-console.log('Hello, TypesScript');
+// console.log('Hello, TypesScript');
 
-function add(a: number, b: number) {
-    return a + b;
+// function add(a: number, b: number) {
+//     return a + b;
+// }
+
+// const sum = add(2, 3)
+//*******************
+
+//boolean
+let muted: boolean = true;
+muted = false;
+muted = "hellostring";
+
+//numeros
+let age = 6;
+let numerador: number = 42;
+let denominador: number = age;
+let resultado = numerador / denominador;
+
+//string
+let nombre: string = 'Richard';
+let saludo = `Me llamo ${nombre}`;
+
+//Arreglos
+let people: string[] = [];
+people = ["Isabel", "Nicole", "Raul"];
+// people.push("9000")
+
+let peopleAndNumbers: Array< string | number > = [];
+peopleAndNumbers.push('Ricardo');
+peopleAndNumbers.push(9001);
+
+// Enum
+
+enum Color {
+    Rojo = "Rojo",
+    Verde = "Verde",
+    Azul = "Azul",
+    Amarillo = "Amarillo",
 }
 
-const sum = add(2, 3)
+let colorFavorito: Color = Color.Verde;
+console.log(`Mi color favorito es: ${colorFavorito}`);
+
+// Any
+let comodin: any = "Joker";
+comodin = { type: 'WildCard'};
+
+// Object
+let someObject: object = { type: 'Wilcard' };
+
+
+
